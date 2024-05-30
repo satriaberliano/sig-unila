319794c40dc46612c1ac479808b09a9e65bd2423
satriaberliano <sanozisb@gmail.com>
2024-05-30 17:41:18 +0700

    fix: width relative

--- components/LandingPage/Banner/Banner.jsx
+++ components/LandingPage/Banner/Banner.jsx
@@ -1,24 +1,24 @@
 // ** Import Next
-import assets from '@/assets/assets'
-import Image from 'next/image'
+import assets from "@/assets/assets";
+import Image from "next/image";
 
 export default function Banner() {
   return (
-    <div>
+    <div className="relative">
       <Image
         src={assets.banner}
         alt="banner"
         className="w-full h-screen object-cover object-center"
         quality={100}
-        placeholder='blur'
+        placeholder="blur"
         priority
       />
       <div className="absolute mt-20 top-0 right-0 bottom-0 left-0 flex justify-center items-center text-white flex-col space-y-6">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center font-semibold uppercase">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl text-center font-semibold">
           Sistem Informasi Geografis <br />
           Universitas Lampung
         </h1>
       </div>
     </div>
-  )
+  );
 }
