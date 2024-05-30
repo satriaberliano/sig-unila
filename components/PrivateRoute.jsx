4d3dbc1613ada160a79bfaba26ea31d4a16fd944
satriaberliano <sanozisb@gmail.com>
2024-05-30 23:12:14 +0700

    fix: static

--- components/PrivateRoute.jsx
+++ components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import supabaseServer from "@/lib/supabaseServer";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function PrivateRoute({ children }) {
-  const supabase = createServerComponentClient({ cookies });
+  // const supabase = createServerComponentClient({ cookies });
 
-  const { data } = await supabase.auth.getSession();
+  const { data } = await supabaseServer().auth.getSession();
 
   if (data.session) {
     redirect("/dashboard");
