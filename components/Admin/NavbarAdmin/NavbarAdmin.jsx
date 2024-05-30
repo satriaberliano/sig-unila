ac191635198dd20ee0b5c6614c212ff02a892de4
satriaberliano <sanozisb@gmail.com>
2024-05-30 17:39:55 +0700

    add: responsive tab

--- components/Admin/NavbarAdmin/NavbarAdmin.jsx
+++ components/Admin/NavbarAdmin/NavbarAdmin.jsx
@@ -1,18 +1,32 @@
-'use client'
+"use client";
 
 // ** Import Icons
 import { useSidebar } from "@/zustand/useSidebar";
+import { FaUserCircle } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoIosArrowForward } from "react-icons/io";
 
 export default function NavbarAdmin() {
-
-  const { setOpen } = useSidebar();
+  const { open, setOpen } = useSidebar();
 
   return (
     <div className="bg-[#0F6EE3] fixed top-0 w-full border-b border-gray-300  p-5 flex justify-between items-center px-10 z-10">
-      <GiHamburgerMenu className="text-white text-2xl cursor-pointer" onClick={setOpen}/>
+      {open ? (
+        <GiHamburgerMenu
+          className="text-white text-2xl cursor-pointer "
+          onClick={setOpen}
+        />
+      ) : (
+        <IoIosArrowForward
+          className="text-white text-2xl cursor-pointer "
+          onClick={setOpen}
+        />
+      )}
 
-      <p className="text-white">Admin</p>
+      <div className="flex justify-between items-center gap-2 text-white">
+        <FaUserCircle />
+        <span className="text-sm">Admin</span>
+      </div>
     </div>
-  )
+  );
 }
