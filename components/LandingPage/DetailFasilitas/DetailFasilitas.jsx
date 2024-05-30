b429718fff605fc8ada020d4e1f4c9f7ce40f4c8
satriaberliano <sanozisb@gmail.com>
2024-05-30 17:41:46 +0700

    fix: detail facility

--- components/LandingPage/DetailFasilitas/DetailFasilitas.jsx
+++ components/LandingPage/DetailFasilitas/DetailFasilitas.jsx
@@ -1,28 +1,72 @@
-import assets from '@/assets/assets'
-import Map from '@/components/Map/Map';
-import Image from 'next/image'
-import Link from 'next/link';
+"use client";
+
+// ** Import Assets
+import assets from "@/assets/assets";
+
+// ** Import Next
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+// ** Import Icons
 import { FaMapMarkerAlt } from "react-icons/fa";
 
-export default function DetailFasilitas() {
+// ** Import Components
+import MapEachFacility from "../MapEachFacility/MapEachFacility";
+
+const DetailFasilitas = ({ facility }) => {
+  // if (facility.length == 0) {
+  //   notFound();
+  // }
+
   return (
-    <div className='space-y-36 px-20 py-40'>
-      <div className='flex justify-center items-center gap-10'>
-        <div className='basis-1/2 flex justify-center items-center'>
-          <Image src={assets.fasilitas} alt='Gambar fasilitas' className='w-[30rem] rounded-lg'/>
-        </div>
-        <div className='basis-1/2 space-y-6'>
-          <h2 className='text-2xl font-semibold'>Fasilitas 1</h2>
-            <Link className='flex items-center gap-3 text-red-700 w-fit' href='https://maps.app.goo.gl/GRNcpv2Sph2PUytk8' target='_blank'>
-              <FaMapMarkerAlt />
-              <p className='text-sm'>Navigasi</p>
-            </Link>
-          <p className='text-sm'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, voluptates perspiciatis nostrum quos earum iste. Molestiae inventore reiciendis beatae aliquam harum nesciunt officiis error sapiente excepturi nemo eveniet qui a, eos necessitatibus maxime, ratione cupiditate possimus quo blanditiis itaque dolor. Id voluptatibus culpa quaerat excepturi provident quam repellat aliquid officiis deleniti, suscipit incidunt totam voluptatum eaque reiciendis ipsa fugiat? Eos quo aliquam temporibus quibusdam, fuga libero laboriosam repudiandae maiores unde dolore quasi aliquid ipsam alias dicta aperiam quisquam non velit nulla quia laborum amet sint voluptatem rem iusto. Facere fuga exercitationem id explicabo dolorum numquam ab laudantium adipisci sapiente voluptatibus.
-          </p>
-        </div>
-      </div>
-      <Map />
-    </div>
-  )
-}
+    <>
+      {facility && facility.length > 0
+        ? facility.map((fasilitas, index) => (
+            <div
+              className="space-y-36 px-14 md:px-20 py-32 md:py-40"
+              key={index}
+            >
+              <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start gap-10 md:gap-20">
+                <div className="basis-1 md:basis-1/2 flex justify-center lg:justify-start items-center w-full">
+                  <Image
+                    src={
+                      fasilitas.url_image
+                        ? fasilitas.url_image
+                        : assets.fasilitas
+                    }
+                    alt={`Gambar fasilitas ${fasilitas.name}`}
+                    className="w-full rounded-lg"
+                    width={500}
+                    height={600}
+                  />
+                </div>
+                <div className="basis-1 md:basis-1/2 space-y-6">
+                  <h2 className="text-2xl font-semibold">{fasilitas.name}</h2>
+                  <Link
+                    className="flex items-center gap-3 text-red-700 w-fit"
+                    href={`https://www.google.com/maps/place/${fasilitas.latitude}+${fasilitas.longitude}`}
+                    target="_blank"
+                    onClick=""
+                  >
+                    <FaMapMarkerAlt />
+                    <p className="text-sm">Navigasi</p>
+                  </Link>
+                  <p className="text-base">{fasilitas.description}</p>
+                </div>
+              </div>
+              <MapEachFacility
+                facilityLat={fasilitas.latitude}
+                facilityLong={fasilitas.longitude}
+                facilityName={fasilitas.name}
+              />
+            </div>
+          ))
+        : notFound()}
+      {/* {params.fasilitasId !== facility.id ? notFound() : ""} */}
+      {/* {facility.id !== params.fasilitasId ? notFound() : ""} */}
+    </>
+  );
+};
+
+export default DetailFasilitas;
