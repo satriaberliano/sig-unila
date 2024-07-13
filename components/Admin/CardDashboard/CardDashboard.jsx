45b8fd6b453bf4fb37eb69df695cfa62e12b3519
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:52:55 +0700

    add: card dashboard

--- components/Admin/CardDashboard/CardDashboard.jsx
+++ components/Admin/CardDashboard/CardDashboard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { RiBuilding4Fill } from "react-icons/ri";
+import Loading from "../Loading/Loading";
 
 const CardDashboard = ({ total, titleCard }) => {
   return (
     <div className="relative overflow-hidden shadow-md max-w-lg rounded-xl bg-[#F1F1F1] py-6 px-7 space-y-4">
-      <h2 className="font-medium text-neutral-700">{titleCard}</h2>
-      <p className="text-xl font-semibold">{total || "-"}</p>
+      <div className="flex flex-col justify-between h-full">
+        <h2 className="font-medium text-neutral-700">{titleCard}</h2>
+        {total ? (
+          <p className="text-xl font-semibold">{total || "-"}</p>
+        ) : (
+          <p className="text-base animate-pulse text-gray-800">Loading...</p>
+        )}
+      </div>
+      {/* <p className="text-xl font-semibold">{total || '-'}</p> */}
       <div className="hidden md:block absolute -bottom-2 right-0">
         <RiBuilding4Fill className="text-[5.5rem] text-neutral-300" />
       </div>
--- components/Admin/MetricCardDashboard/MetricCardDashboard.jsx
+++ components/Admin/MetricCardDashboard/MetricCardDashboard.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+/* eslint-disable @next/next/no-async-client-component */
+"use client";
 
-const MetricCardDashboard = () => {
-  return <div>MetricCardDashboard</div>;
+import dynamic from "next/dynamic";
+import "chart.js/auto";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+import { getMonth, parseISO } from "date-fns";
+import { Line } from "react-chartjs-2";
+import Loading from "../Loading/Loading";
+
+// const Line = dynamic(() => import("react-chartjs-2").then((mod) => mod.Line), {
+//   ssr: false,
+//   loading: () => <Loading width={10} />,
+// });
+
+const supabase = createClientComponentClient();
+
+const MetricCardDashboard = ({ title }) => {
+  const [facilitiesData, setFacilitiesData] = useState({
+    labels: [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ],
+    datasets: [
+      {
+        label: "Data Fasilitas",
+        data: Array(12).fill(0),
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const facilities = await fetchFacilitiesData();
+      const groupedData = groupByMonth(facilities);
+      setFacilitiesData((prevState) => ({
+        ...prevState,
+        datasets: [
+          {
+            ...prevState.datasets[0],
+            data: groupedData,
+          },
+        ],
+      }));
+    };
+    fetchData();
+  }, []);
+
+  const fetchFacilitiesData = async () => {
+    const { data, error } = await supabase
+      .from("fasilitas")
+      .select("created_at");
+
+    if (error) {
+      console.error("Error fetching data:", error);
+      return [];
+    }
+
+    return data;
+  };
+
+  const groupByMonth = (data) => {
+    const grouped = Array(12).fill(0);
+
+    data.forEach((facility) => {
+      const date = parseISO(facility.created_at);
+      const month = getMonth(date);
+      grouped[month]++;
+    });
+
+    return grouped;
+  };
+
+  return (
+    <div className="flex flex-col shadow-md w-full rounded-xl bg-[#F1F1F1] py-6 px-7 space-y-4 text-black">
+      <h2 className="text-lg font-medium">{title}</h2>
+      {facilitiesData ? <Line data={facilitiesData} /> : <Loading></Loading>}
+      {/* <Line data={facilitiesData} /> */}
+    </div>
+  );
 };
 
 export default MetricCardDashboard;
