f92d37d2e7a501e178280063f76e043e8c86ba7d
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:54:29 +0700

    fix: height page

--- app/(admin)/dashboard/page.jsx
+++ app/(admin)/dashboard/page.jsx
@@ -1,10 +1,10 @@
-import HeadDashboard from '@/components/Admin/HeadDashboard/HeadDashboard'
-import React from 'react'
+import HeadDashboard from "@/components/Admin/HeadDashboard/HeadDashboard";
+import React from "react";
 
 export default function Dashboard() {
   return (
-    <div className='px-10 py-16 h-screen'>
+    <div className="px-10 py-16 h-full">
       <HeadDashboard />
     </div>
-  )
+  );
 }
