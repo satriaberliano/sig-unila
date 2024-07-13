7d046fe1a9151613153fcbc392f85bd5395cfed3
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:53:15 +0700

    add: dynamic

--- components/Admin/HeadDashboard/HeadDashboard.jsx
+++ components/Admin/HeadDashboard/HeadDashboard.jsx
@@ -1,35 +1,58 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
+import dayjs from "dayjs";
 import CardDashboard from "../CardDashboard/CardDashboard";
 import MetricCardDashboard from "../MetricCardDashboard/MetricCardDashboard";
+import supabaseServer from "@/lib/supabaseServer";
+import dynamic from "next/dynamic";
+import Loading from "../Loading/Loading";
+
+const Map = dynamic(() => import("@/components/Map/Map"), {
+  ssr: false,
+  loading: () => <Loading></Loading>,
+});
 
 const HeadDashboard = async () => {
-  const supabase = createServerComponentClient({ cookies });
+  // const supabase = createServerComponentClient({ cookies });
 
-  let { data: fasilitas, error } = await supabase.from("fasilitas").select();
+  let { data: fasilitas, error } = await supabaseServer()
+    .from("fasilitas")
+    .select();
+
+  const latLongData = fasilitas.map((facility) => {
+    return [facility.latitude, facility.longitude];
+  });
+
+  // console.log(latLongData);
 
   return (
-    <div className="space-y-2 mb-14">
+    <div className="space-y-2">
       <h1 className="text-3xl mb-10 font-medium">Dashboard</h1>
       <div className="grid grid-cols-3 gap-5 w-full">
         <CardDashboard
           total={fasilitas.length}
           titleCard={"Jumlah Fasilitas"}
         />
-        <CardDashboard total={""} titleCard={"Jumlah Lokasi"} />
-        <CardDashboard total={""} titleCard={"No Information"} />
+        <CardDashboard
+          total={latLongData.length}
+          titleCard={"Jumlah Lokasi Pin Point"}
+        />
+        <CardDashboard
+          total={""}
+          titleCard={"Jumlah Fasilitas dalam Pemeliharaan (Belum Fix)"}
+        />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pt-5">
         {/* <MetricCardDashboard /> */}
-        <div className="flex flex-col shadow-md w-full rounded-xl bg-[#F1F1F1] py-6 px-7 space-y-4">
-          <h2 className="text-lg font-medium">Overview Metrics</h2>
-          <p>Unknown value</p>
-        </div>
-        <div className="flex flex-col shadow-md w-full rounded-xl bg-[#F1F1F1] py-6 px-7 space-y-4">
-          <h2 className="text-lg font-medium">Overview Metrics</h2>
-          <p>Unknown value</p>
-        </div>
+        <MetricCardDashboard title={"Metrik Penambahan Data Fasilitas"} />
+        <MetricCardDashboard
+          title={"Metrik Fasilitas dalam Pemeliharaan (Belum Fix)"}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-5 pt-5">
+        <Map facilities={null} />
       </div>
     </div>
   );
