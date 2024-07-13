f534a658b7e70ef491cb18558b3ff86af67bf15f
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:47:02 +0700

    edit: supabase-ssr

--- utils/supabase/middleware.js
+++ utils/supabase/middleware.js
@@ -1,21 +1,83 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function middleware(req = NextRequest) {
+//   const res = NextResponse.next();
+//   const supabase = createMiddlewareClient({ req, res });
+//   const {
+//     data: { session },
+//     error,
+//   } = await supabase.auth.getSession();
+
+//   if (!session) {
+//     return NextResponse.rewrite(new URL("masuk", req.url));
+//   }
+
+//   return res;
+// }
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
+
+import { createServerClient } from "@supabase/ssr";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req = NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  const {
-    data: { session },
-    error,
-  } = await supabase.auth.getSession();
+export const createClient = (request = NextRequest) => {
+  // Create an unmodified response
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-  if (!session) {
-    return NextResponse.rewrite(new URL("masuk", req.url));
-  }
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name, value, options) {
+          // If the cookie is updated, update the cookies for the request and response
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name, options) {
+          // If the cookie is removed, update the cookies for the request and response
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
 
-  return res;
-}
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  return { supabase, response };
 };
