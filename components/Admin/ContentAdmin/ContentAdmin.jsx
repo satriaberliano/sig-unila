47828d953ef785c0b3e8468de0a3b2d49c3edfee
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:55:24 +0700

    fix: height page

--- components/Admin/ContentAdmin/ContentAdmin.jsx
+++ components/Admin/ContentAdmin/ContentAdmin.jsx
@@ -1,20 +1,24 @@
-"use client"
+"use client";
 
-import React from 'react'
-import NavLeft from '../NavLeft/NavLeft'
-import { useModalFacility } from '@/zustand/useModalFacility'
-import ModalAddFacility from '../ModalAddFacility/ModalAddFacility';
-import { useSidebar } from '@/zustand/useSidebar';
-import ModalEditFacility from '../ModalEditFacility/ModalEditFacility';
+import React from "react";
+import NavLeft from "../NavLeft/NavLeft";
+import { useModalFacility } from "@/zustand/useModalFacility";
+import ModalAddFacility from "../ModalAddFacility/ModalAddFacility";
+import { useSidebar } from "@/zustand/useSidebar";
+import ModalEditFacility from "../ModalEditFacility/ModalEditFacility";
 
 export default function ContentAdmin({ children }) {
   const { facility, isEdit: isEditFacility } = useModalFacility();
   const { open } = useSidebar();
 
   return (
-    <div className='relative flex mt-10 bg-white'>
+    <div className="relative flex mt-10 bg-white">
       <div className="mt-6">
-        <div className={`fixed w-[16rem] ${ open ? "transform-none" : "" } -translate-x-full duration-700 transition-transform z-10 border-r-2`}>
+        <div
+          className={`fixed w-[16rem] ${
+            open ? "transform-none" : ""
+          } -translate-x-full duration-700 transition-transform z-10 border-r-2`}
+        >
           <NavLeft />
         </div>
       </div>
@@ -30,14 +34,21 @@ export default function ContentAdmin({ children }) {
 
       <div
         className={` ${
-          !isEditFacility ? "hidden" : "fixed -mt-10 w-screen bg-slate-900/75 z-20"
+          !isEditFacility
+            ? "hidden"
+            : "fixed -mt-10 w-screen bg-slate-900/75 z-20"
         }`}
       >
         <ModalEditFacility />
       </div>
 
-      <div className={`${ open ? "lg:pl-[16rem]" : "pl-0"} h-screen w-full duration-700 ease-in-out`}>{children}</div>
-
+      <div
+        className={`${
+          open ? "lg:pl-[16rem]" : "pl-0"
+        } h-full w-full duration-700 ease-in-out`}
+      >
+        {children}
+      </div>
     </div>
-  )
+  );
 }
