7ab0f230c3d7e15e64bc4d38746c1375e2a03811
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:53:31 +0700

    fix: loading component

--- components/Admin/Loading/Loading.jsx
+++ components/Admin/Loading/Loading.jsx
@@ -1,6 +1,6 @@
-const Loading = ({ width = 7 }) => {
+const Loading = (width = 7) => {
   return (
-    <div className="flex justify-center items-center">
+    <div className={`flex justify-center items-center`}>
       {/* <div
         className={`border-r-[2px] border-white w-${width} h-${width} aspect-square rounded-full text-white animate-spin-slow`}
       >
@@ -8,7 +8,7 @@ const Loading = ({ width = 7 }) => {
       </div> */}
       <svg
         aria-hidden="true"
-        class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className={`w-7 h-7 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
