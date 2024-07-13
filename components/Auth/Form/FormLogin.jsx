97016573234946f99f85607f131268e550b5f50e
satriaberliano <sanozisb@gmail.com>
2024-07-14 00:51:22 +0700

    edit: property label

--- components/Auth/Form/FormLogin.jsx
+++ components/Auth/Form/FormLogin.jsx
@@ -84,7 +84,7 @@ const FormLogin = () => {
         />
         <label
           className="mb-1 flex items-center absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          for="email_input"
+          htmlFor="email_input"
         >
           Email
           {errors?.email && (
@@ -103,7 +103,7 @@ const FormLogin = () => {
         />
         <label
           className="mb-1 flex items-center absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          for="password_input"
+          htmlFor="password_input"
         >
           Password
           {errors?.password && (
