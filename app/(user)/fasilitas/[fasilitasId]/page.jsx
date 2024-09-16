9e87cf3ebe5b3a0fda19b80630a14286abba3f61
satriaberliano <sanozisb@gmail.com>
2024-09-16 16:20:23 +0700

    add: caching data

--- app/(user)/fasilitas/[fasilitasId]/page.jsx
+++ app/(user)/fasilitas/[fasilitasId]/page.jsx
@@ -1,17 +1,22 @@
+"use client";
+
 import DetailFasilitas from "@/components/LandingPage/DetailFasilitas/DetailFasilitas";
 import { supabase } from "@/lib/supabase";
+import { useFetchData } from "@/zustand/useFetchData";
 import { notFound, usePathname } from "next/navigation";
 import React from "react";
 
-const FasilitasLain = async ({ params }) => {
-  const { data: facility, error } = await supabase
-    .from("fasilitas")
-    .select("*")
-    .eq("id", params.fasilitasId);
+const FasilitasLain = ({ params }) => {
+  // const { data: facility, error } = await supabase
+  //   .from("fasilitas")
+  //   .select("*")
+  //   .eq("id", params.fasilitasId);
+  const { facility } = useFetchData();
 
   return (
     <>
-      {facility ? (
+      {/* {console.log(facility)} */}
+      {params.fasilitasId == facility.id ? (
         <DetailFasilitas params={params} facility={facility} />
       ) : (
         notFound()
