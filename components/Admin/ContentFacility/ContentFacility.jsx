c260a952079bb1cdf1a54a05d0bc2295558b0c8e
satriaberliano <sanozisb@gmail.com>
2024-09-16 16:21:08 +0700

    add: some input

--- components/Admin/ContentFacility/ContentFacility.jsx
+++ components/Admin/ContentFacility/ContentFacility.jsx
@@ -10,9 +10,14 @@ import { useModalFacility } from "@/zustand/useModalFacility";
 import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import Link from "next/link";
+import { useImages } from "@/zustand/useImages";
+import { MdOutlineArrowOutward } from "react-icons/md";
 
 export const ContentFacility = ({ facilities }) => {
   const { setFacility, setData, setIsEdit } = useModalFacility();
+  const { setImage, setImages } = useImages();
+
   const router = useRouter();
   const supabase = createClientComponentClient();
 
@@ -50,96 +55,143 @@ export const ContentFacility = ({ facilities }) => {
             text: error.message,
           });
         }
-
-        // router.refresh();
       }
     });
   };
 
   const onEditHandler = (val) => {
     setData(val);
-    setIsEdit();
+    router.push(`/data-fasilitas/sunting-fasilitas/${val.id}`);
+    // setIsEdit();
+  };
+
+  const onImageHandler = (val) => {
+    setImages(val);
+    setImage();
   };
 
   return (
-    <main className="w-full mb-4">
+    <main className={`w-full mb-4 `}>
       <div className="flex justify-between mb-2">
         <h2 className="font-medium text-lg">Data Fasilitas</h2>
         {/* Tambahin Tombol Add Fasilitas */}
-        <button
+        <Link
           className="flex items-center text-xs gap-2 rounded-md bg-[#0F6EE3] text-white p-2"
-          onClick={setFacility}
+          // onClick={setFacility}
+          href="/data-fasilitas/tambah-fasilitas"
         >
           <IoAddOutline />
           <span>Tambah</span>
-        </button>
+        </Link>
       </div>
 
-      <table className="w-full table-auto mt-4">
-        <tbody className="">
-          <tr className="py-10 text-sm">
-            <th>No</th>
-            <th>Gambar</th>
-            <th>Nama</th>
-            <th>Deskripsi</th>
-            <th>Koordinat</th>
-            <th>Jam Operasional</th>
-            <th>Kontak</th>
-            <th>Akses</th>
-            <th>Aksi</th>
-          </tr>
-          {/* {facilities && facilities.length > 0 ? (
-            <> */}
-          {facilities.map((fasilitas, index) => (
-            <tr key={index} className="p-2 bg-gray-200">
-              <td className="text-sm text-center">{index + 1}</td>
-              <td>
-                <Image
-                  alt={`Gambar Fasilitas ${fasilitas.name}`}
-                  src={
-                    fasilitas.url_image
-                      ? fasilitas.url_image
-                      : assets.defaultImage
-                  }
-                  className="mx-auto rounded-md col-start-2 col-end-4"
-                  width={75}
-                  height={0}
-                />
-              </td>
-              <td className="text-xs">{fasilitas.name || "-"}</td>
-              <td className="text-xs ">{fasilitas.description || "-"}</td>
-              <td className="text-xs">
-                <p>Lat: {fasilitas.latitude || "-"},</p>
-                <p>Long: {fasilitas.longitude || "-"}</p>
-              </td>
-              <td className="text-xs">
-                <p>{fasilitas.jam_buka || "-"} s.d</p>
-                <p>{fasilitas.jam_tutup || "-"}</p>
-              </td>
-              <td className="text-xs text-center">{fasilitas.kontak || "-"}</td>
-              <td className="text-xs text-center">{fasilitas.akses || "-"}</td>
-              <td className="space-y-2 text-center align-middle">
-                <button
-                  className="bg-[#2cbc35] rounded-md p-2 text-white"
-                  onClick={() => onEditHandler(fasilitas)}
-                >
-                  <HiOutlinePencil />
-                </button>
-                <button
-                  className="bg-[#EF4444] rounded-md p-2 text-white"
-                  onClick={() => onDeleteHandler(fasilitas)}
-                >
-                  <HiOutlineTrash />
-                </button>
-              </td>
-            </tr>
-          ))}
-          {/* </>
-          ) : (
-            <p>Tidak ada data fasilitas</p>
-          )} */}
-        </tbody>
-      </table>
+      {facilities && facilities.length > 0 ? (
+        <>
+          <table className="w-full table-auto mt-4">
+            <tbody className="">
+              <tr className="py-10 text-sm">
+                <th>No</th>
+                <th>Gambar</th>
+                <th>Nama</th>
+                <th>Deskripsi</th>
+                <th>Koordinat</th>
+                <th>Jam Operasional</th>
+                <th>Kontak</th>
+                <th>Akses</th>
+                <th>Aksi</th>
+              </tr>
+
+              {facilities.map((fasilitas, index) => (
+                <tr key={index} className="p-2 bg-gray-200">
+                  <td className="text-sm text-center">{index + 1}</td>
+                  <td>
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => onImageHandler(fasilitas.url_image)}
+                    >
+                      <Image
+                        alt={`Gambar Fasilitas ${fasilitas.name}`}
+                        src={
+                          fasilitas.url_image
+                            ? fasilitas.url_image
+                            : assets.defaultImage
+                        }
+                        className="rounded-sm mx-auto"
+                        width={40}
+                        height={40}
+                      />
+                    </div>
+                  </td>
+                  <td className="text-xs">{fasilitas.name || "-"}</td>
+                  <td
+                    className="text-xs line-clamp-5 h-24 border-1"
+                    dangerouslySetInnerHTML={{
+                      __html: `${fasilitas.description}`,
+                    }}
+                  >
+                    {/* {fasilitas.description || "-"} */}
+                  </td>
+                  <td className="text-xs">
+                    <div className="flex flex-col items-center justify-center">
+                      <div>
+                        <p>Lat: {fasilitas.latitude || "-"},</p>
+                        <p>Long: {fasilitas.longitude || "-"}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="text-xs">
+                    <div className="flex items-center justify-center">
+                      {fasilitas.jam_operasional.map((hour, index) => {
+                        return (
+                          <div key={index}>
+                            <p>
+                              Jam : {hour.jam_buka} - {hour.jam_tutup}
+                            </p>
+                            <p>
+                              Hari : {hour.hari_awal} s.d {hour.hari_akhir}
+                            </p>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </td>
+                  <td className="text-xs">
+                    <div className="flex flex-col items-center justify-center">
+                      <div>
+                        <p>Nama: {fasilitas.kontak?.nama_kontak || "-"}</p>
+                        <p>Nomor: {fasilitas.kontak?.nomor_telepon || "-"}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="text-xs text-center">
+                    {fasilitas.akses || "-"}
+                  </td>
+                  <td className="space-y-2 text-center align-middle ">
+                    <div className="flex flex-col items-center gap-y-2">
+                      <button
+                        className="bg-[#2cbc35] rounded-md p-2 text-white"
+                        onClick={() => onEditHandler(fasilitas)}
+                      >
+                        <HiOutlinePencil />
+                      </button>
+                      <button
+                        className="bg-[#EF4444] rounded-md p-2 text-white"
+                        onClick={() => onDeleteHandler(fasilitas)}
+                      >
+                        <HiOutlineTrash />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <div className="flex items-center justify-center italic text-sm p-10">
+          Tidak ada data fasilitas untuk ditampilkan
+        </div>
+      )}
 
       {/* <div className="w-full space-y-4">
         <div className="grid grid-cols-10 text-center text-sm font-medium gap-2 mt-4">
