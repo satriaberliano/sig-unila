b44e0637daa3f3dc82a05472f8f22df775fe4ccd
satriaberliano <sanozisb@gmail.com>
2024-09-16 16:23:07 +0700

    add: client ip address

---
+++ utils/getClientIp.js
@@ -0,0 +1,7 @@
+export function getClientIp(req) {
+  const forwarded = req.headers["x-forwarded-for"];
+  const ip = forwarded
+    ? forwarded.split(/, /)[0]
+    : req.connection.remoteAddress;
+  return ip;
+}
