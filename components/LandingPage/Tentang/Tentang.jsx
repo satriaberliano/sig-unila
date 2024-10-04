20346d36b1cb46c8d2f1da2689a131b3689daa7c
satriaberliano <sanozisb@gmail.com>
2024-10-05 02:18:33 +0700

    add: button to tentang page

--- components/LandingPage/Tentang/Tentang.jsx
+++ components/LandingPage/Tentang/Tentang.jsx
@@ -1,20 +1,29 @@
-import assets from '@/assets/assets'
-import Image from 'next/image'
+import assets from "@/assets/assets";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Tentang() {
   return (
     <div className="flex justify-center items-center p-20 gap-14 flex-col-reverse md:flex-row md:p-24 md:gap-32 lg:py-36 lg:px-32 lg:gap-60">
-      <h4 className="text-base text-center md:text-lg md:text-left">
-        SIG UNILA merupakan sebuah website yang menyediakan informasi mengenai
-        fasilitas yang ada pada Universitas Lampung. SIG UNILA juga
-        menyediakan sebuah peta digital Universitas Lampung dengan lokasi dari
-        tiap fasilitas
-      </h4>
+      <div className="flex flex-col items-center md:items-start gap-y-12">
+        <h4 className="text-base text-center md:text-lg md:text-left">
+          SIG UNILA merupakan sebuah website yang menyediakan informasi mengenai
+          fasilitas yang ada pada Universitas Lampung. SIG UNILA juga
+          menyediakan sebuah peta digital Universitas Lampung dengan lokasi dari
+          tiap fasilitas
+        </h4>
+        <Link
+          href="/tentang"
+          className="px-3 py-2 bg-[#0F6EE3] text-white w-fit rounded-md text-sm"
+        >
+          Selengkapnya
+        </Link>
+      </div>
       <Image
         src={assets.logoUnila}
         alt="universitas lampung logo"
         className="w-4/12 md:w-3/12 lg:w-2/12"
       />
     </div>
-  )
+  );
 }
