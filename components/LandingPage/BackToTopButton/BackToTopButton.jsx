13d9d265efd4bf729906e546ccbcab7a9c417d77
satriaberliano <sanozisb@gmail.com>
2024-10-05 02:18:06 +0700

    add: back to top button

---
+++ components/LandingPage/BackToTopButton/BackToTopButton.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const BackToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-10 right-10 p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-all duration-300"
+          aria-label="Back to top"
+          name="Back to Top"
+        >
+          <FaArrowUp size={24} />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default BackToTopButton;
