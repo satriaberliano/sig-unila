4082be157887f8d8503b8b7976b03a00df4b0379
satriaberliano <sanozisb@gmail.com>
2024-10-23 19:25:22 +0700

    change: data cache

--- app/(user)/fasilitas/[fasilitasId]/page.jsx
+++ app/(user)/fasilitas/[fasilitasId]/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import DetailFasilitas from "@/components/LandingPage/DetailFasilitas/DetailFasilitas";
 import { supabase } from "@/lib/supabase";
@@ -6,21 +6,37 @@ import { useFetchData } from "@/zustand/useFetchData";
 import { notFound, usePathname } from "next/navigation";
 import React from "react";
 
-const FasilitasLain = ({ params }) => {
-  // const { data: facility, error } = await supabase
-  //   .from("fasilitas")
-  //   .select("*")
-  //   .eq("id", params.fasilitasId);
-  const { facility } = useFetchData();
+const FasilitasLain = async ({ params }) => {
+  const { data: facility, error } = await supabase
+    .from("fasilitas")
+    .select(
+      ` id,
+        name,
+        description,
+        latitude,
+        longitude,
+        url_image,
+        akses,
+        fakultas,
+        kontak (
+          nama_kontak,
+          nomor_telepon
+        ),
+        jam_operasional (
+          id_jam,
+          hari_awal,
+          hari_akhir,
+          jam_buka,
+          jam_tutup
+        )`
+    )
+    .eq("id", params.fasilitasId);
+  // const { facility } = useFetchData();
 
   return (
     <>
-      {/* {console.log(facility)} */}
-      {params.fasilitasId == facility.id ? (
-        <DetailFasilitas
-          // params={params}
-          facility={facility}
-        />
+      {facility ? (
+        <DetailFasilitas params={params} facility={facility} />
       ) : (
         notFound()
       )}
